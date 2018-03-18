2376565c8d5cd3f47c6b4dc2da7f484387188ebb
mikeysax <michaelmerrittfarrell@gmail.com>
2018-03-18 14:50:22 -0400

    trailing commas added and config for prettierrc added to .gitignore.

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -11,63 +11,63 @@ import ClubsScreen from '../screens/ClubsScreen';
 import NoticesScreen from '../screens/NoticesScreen';
 
 export default TabNavigator(
-  {
-    Home: {
-      screen: HomeScreen
-    },
-    Artists: {
-      screen: ArtistsScreen
-    },
-    Clubs: {
-      screen: ClubsScreen
-    },
-    Notices: {
-      screen: NoticesScreen
-    }
-  },
-  {
-    navigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused }) => {
-        const { routeName } = navigation.state;
-        let iconName;
-        switch (routeName) {
-          case 'Home':
-            iconName =
-              Platform.OS === 'ios'
-                ? `ios-information-circle${focused ? '' : '-outline'}`
-                : 'md-information-circle';
-            break;
-          case 'Artists':
-            iconName =
-              Platform.OS === 'ios'
-                ? `ios-link${focused ? '' : '-outline'}`
-                : 'md-link';
-            break;
-          case 'Clubs':
-            iconName =
-              Platform.OS === 'ios'
-                ? `ios-link${focused ? '' : '-outline'}`
-                : 'md-link';
-            break;
-          case 'Notices':
-            iconName =
-              Platform.OS === 'ios'
-                ? `ios-options${focused ? '' : '-outline'}`
-                : 'md-options';
-        }
-        return (
-          <Ionicons
-            name={iconName}
-            size={28}
-            style={{ marginBottom: -3 }}
-            color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
-          />
-        );
-      }
-    }),
-    tabBarComponent: TabBarBottom,
-    tabBarPosition: 'bottom',
-    animationEnabled: false,
-    swipeEnabled: false
-  }
+	{
+		Home: {
+			screen: HomeScreen,
+		},
+		Artists: {
+			screen: ArtistsScreen,
+		},
+		Clubs: {
+			screen: ClubsScreen,
+		},
+		Notices: {
+			screen: NoticesScreen,
+		},
+	},
+	{
+		navigationOptions: ({ navigation }) => ({
+			tabBarIcon: ({ focused }) => {
+				const { routeName } = navigation.state;
+				let iconName;
+				switch (routeName) {
+					case 'Home':
+						iconName =
+							Platform.OS === 'ios'
+								? `ios-information-circle${focused ? '' : '-outline'}`
+								: 'md-information-circle';
+						break;
+					case 'Artists':
+						iconName =
+							Platform.OS === 'ios'
+								? `ios-link${focused ? '' : '-outline'}`
+								: 'md-link';
+						break;
+					case 'Clubs':
+						iconName =
+							Platform.OS === 'ios'
+								? `ios-link${focused ? '' : '-outline'}`
+								: 'md-link';
+						break;
+					case 'Notices':
+						iconName =
+							Platform.OS === 'ios'
+								? `ios-options${focused ? '' : '-outline'}`
+								: 'md-options';
+				}
+				return (
+					<Ionicons
+						name={iconName}
+						size={28}
+						style={{ marginBottom: -3 }}
+						color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+					/>
+				);
+			},
+		}),
+		tabBarComponent: TabBarBottom,
+		tabBarPosition: 'bottom',
+		animationEnabled: false,
+		swipeEnabled: false,
+	}
 );
