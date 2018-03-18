a0b5a83bf18a3bb6599e59fedf8dc30297a987c6
MrMonsk <yowonyoon@gmail.com>
2018-03-18 15:43:50 -0400

    add native-base, create rounded button, sneak asset

---
+++ components/ui/RoundedButton.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+import { Button } from 'native-base';
+
+export const RoundedButton = (props) => (
+  <Button
+    style={styles.roundedButton}
+    onPress={props.handlePress}
+  >
+    <Text style={styles.buttonText}>{props.text}</Text>
+  </Button>
+);
+
+const styles= StyleSheet.create({
+  roundedButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#00b2ee',
+    borderRadius: 50,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    padding: '10%',
+  },
+});
