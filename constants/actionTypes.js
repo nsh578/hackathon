c7989092c5ccde7116b7af087523e72e48704c74
Denis Olsem <dols3m@gmail.com>
2018-03-20 18:43:01 -0400

    Added an extra level to the actionTypes object to separate modules (for import convenience)

--- constants/actionTypes.js
+++ constants/actionTypes.js
@@ -1,9 +1,12 @@
 export default {
   // Auth actions:
-  START_AUTH = 'app/auth/START_AUTH',
-  AUTH_COMPLETE = 'app/auth/AUTH_COMPLETE',
-  AUTH_FAILED = 'app/auth/AUTH_FAILED',
-  START_SIGNUP = 'app/auth/START_SIGNUP',
-  SIGNUP_COMPLETE = 'app/auth/SIGNUP_COMPLETE',
-  SIGNUP_FAILED = 'app/auth/SIGNUP_FAILED',
+  auth: {
+    START_AUTH: 'app/auth/START_AUTH',
+    AUTH_COMPLETE: 'app/auth/AUTH_COMPLETE',
+    AUTH_FAILED: 'app/auth/AUTH_FAILED',
+    START_SIGNUP: 'app/auth/START_SIGNUP',
+    SIGNUP_COMPLETE: 'app/auth/SIGNUP_COMPLETE',
+    SIGNUP_FAILED: 'app/auth/SIGNUP_FAILED',
+  },
+
 }
