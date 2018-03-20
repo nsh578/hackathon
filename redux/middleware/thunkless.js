d7a44b6c6b679cef36b98079820d7e1af0bcfdef
Denis Olsem <dols3m@gmail.com>
2018-03-20 18:41:47 -0400

    Fixed errors in thunkless middleware and rewrote some of its code

--- redux/middleware/thunkless.js
+++ redux/middleware/thunkless.js
@@ -1,43 +1,71 @@
-const BLOCKING = Symbol('THUNKLESS_BLOCKING');
-const WITH_STATUS = Symbol('THUNKLESS_WITH_STATUS');
-const MULTI_ACTION = Symbol('THUNKLESS_MULTI_ACTION');
+const actionStatus = {
+  BUSY: Symbol('THUNKLESS_ACTION_STATUS_BUSY'),
+  SUCCESS: Symbol('THUNKLESS_ACTION_STATUS_SUCCESS'),
+  FAILURE: Symbol('THUNKLESS_ACTION_STATUS_FAILURE'),
+};
 
-const actionsFromResults = (thunklessFlags, types, resultKey, action) => (results) => {
-  if (!thunklessFlags.has(MULTI_ACTION)) {
+const BLOCKING = Symbol('THUNKLESS_FLAG_BLOCKING');
+const WITH_STATUS = Symbol('THUNKLESS_FLAG_WITH_STATUS');
+const MULTI_ACTION = Symbol('THUNKLESS_FLAG_MULTI_ACTION');
+
+const actionsFromResults =
+(next, flags, types, resultKey, action) => (results) => {
+  if (!flags.has(MULTI_ACTION) || !Array.isArray(results)) {
     return next({ [resultKey]: results, type: types, ...action });
-  } else {
-    for (let i = 0; i < results.length-1; i++) {
-      next({ [resultKey]: results[i], type: types[i], ...action });
-    }
-    return next({ [resultKey]: results[i], type: types[i], ...action });
   }
-}
 
-const middleware = store => next => action => {
-  const { thunklessFlags = new Set(), promise, type, ...action } = action;
-  const { statusSelector, statusTypes: [BUSY, SUCCESS, FAILURE], ...action } = action;
+  let i = 0;
+  for (; i < results.length - 1; i += 1) {
+    next({ [resultKey]: results[i], type: types[i], ...action });
+  }
+
+  return next({ [resultKey]: results[i], type: types[i], ...action });
+};
+
+const middleware = store => next => (action) => {
+  // eslint-disable-next-line one-var,one-var-declaration-per-line
+  let flags, promise, type, statusSelector;
+  ({ /* eslint-disable prefer-const,no-param-reassign */
+    flags = new Set(),
+    promise,
+    type,
+    statusSelector,
+    ...action
+  } = action); /* eslint-enable */
   if (!promise) {
     return next(action);
   }
 
-  if (!thunklessFlags.has(WITH_STATUS)) {
-    const types = thunklessFlags.has(MULTI_ACTION) ? type : [type];
+  if (!flags.has(WITH_STATUS)) {
     return promise
-      .then(actionsFromResults(thunklessFlags, types, 'result', action))
-      .catch(actionsFromResults(thunklessFlags, types, 'error', action));
+      .then(actionsFromResults(flags, type, 'payload', action))
+      .catch(actionsFromResults(flags, type, 'error', action));
   }
 
-  if (thunklessFlags.has(BLOCKING) && statusSelector) {
+  const [startActionType, successActionType, failActionType] = type;
+
+  if (flags.has(BLOCKING) && statusSelector) {
     const currentStatus = statusSelector(store.getState());
-    if (currentStatus === BUSY) {
-      return next(action);
+    if (currentStatus === actionStatus.BUSY) {
+      return null;
     }
   }
 
-  next({ type: BUSY, ...action });
+  if (!flags.has(MULTI_ACTION) || !Array.isArray(startActionType)) {
+    next({ type: startActionType, ...action });
+  } else {
+    startActionType.forEach(t => next({ type: t, ...action }));
+  }
+
   return promise
-    .then(actionsFromResults(thunklessFlags, SUCCESS, 'result', action))
-    .catch(actionsFromResults(thunklessFlags, FAILURE, 'error', action));
+    .then(actionsFromResults(flags, successActionType, 'payload', action))
+    .catch(actionsFromResults(flags, failActionType, 'error', action));
 }
 
-export default { middleware, BLOCKING, WITH_STATUS, MULTI_ACTION };
+export default {
+  middleware,
+  actionStatus,
+  BLOCKING,
+  WITH_STATUS,
+  MULTI_ACTION,
+};
