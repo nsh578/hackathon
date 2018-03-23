2cf49f9e8d789d2506360a5410f717c70d17a6b2
Denis Olsem <dols3m@gmail.com>
2018-03-23 02:49:52 -0400

    Implemented post actions/reducers/selectors

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import user from './user';
 import auth from './auth';
 import feed from './feed';
+import post from './post';
 import error from './error';
 
 const rootReducer = combineReducers({
   user,
   auth,
   feed,
+  post,
   error,
 });
 
---
+++ redux/reducers/post.js
@@ -0,0 +1,74 @@
+import { Map } from 'immutable';
+
+import thunkless from '../middleware/thunkless';
+import { post as actionTypes } from '../../constants/actionTypes';
+
+const initialState = Map();
+
+export default (state = initialState, { payload, stream, ...action }) => {
+  switch (action.type) {
+    case actionTypes.START_FETCHING_ACTIVITIES:
+      return state.mergeDeep({
+        [stream.feedUrl]: {
+          fetchActivitiesStatus: thunkless.actionStatus.BUSY,
+        },
+      });
+    case actionTypes.FETCH_ACTIVITIES_SUCCESS:
+      return state.mergeDeep({
+        [stream.feedUrl]: {
+          fetchActivitiesStatus: thunkless.actionStatus.SUCCESS,
+        },
+      });
+    case actionTypes.FETCH_ACTIVITIES_FAILED:
+      return state.mergeDeep({
+        [stream.feedUrl]: {
+          fetchActivitiesStatus: thunkless.actionStatus.FAILURE,
+        },
+      });
+    case actionTypes.ADD_POST:
+      return state.mergeDeep({
+        [stream.feedUrl]: {
+          posts: { [payload.timestamp]: payload },
+        },
+      });
+    case actionTypes.START_UPLOADING_POST:
+      return state.mergeDeep({
+        [stream.feedUrl]: {
+          uploadPostStatus: thunkless.actionStatus.BUSY,
+        },
+      });
+    case actionTypes.UPLOAD_POST_SUCCESS:
+      return state.mergeDeep({
+        [stream.feedUrl]: {
+          uploadPostStatus: thunkless.actionStatus.SUCCESS,
+        },
+      });
+    case actionTypes.UPLOAD_POST_FAILED:
+      return state.mergeDeep({
+        [stream.feedUrl]: {
+          uploadPostStatus: thunkless.actionStatus.FAILURE,
+        },
+      });
+    case actionTypes.START_FETCHING_POST:
+      return state.mergeDeep({
+        [stream.feedUrl]: {
+          fetchPostStatus: thunkless.actionStatus.BUSY,
+        },
+      });
+    case actionTypes.FETCH_POST_SUCCESS:
+      return state.mergeDeep({
+        [stream.feedUrl]: {
+          fetchPostStatus: thunkless.actionStatus.SUCCESS,
+          posts: { [payload.data.timestamp]: payload.data },
+        },
+      });
+    case actionTypes.FETCH_POST_FAILED:
+      return state.mergeDeep({
+        [stream.feedUrl]: {
+          fetchPostStatus: thunkless.actionStatus.FAILURE,
+        },
+      });
+    default:
+      return state;
+  }
+}
